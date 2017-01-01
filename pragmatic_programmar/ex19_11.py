70c3c493ac531af6e4a64b25ff2e071d3b00a83b
wikibootup <wikibootup@gmail.com>
2017-01-01 14:54:06 +0900

    update enumOp : gen c file

--- pragmatic_programmar/ex19_11.py
+++ pragmatic_programmar/ex19_11.py
@@ -6,3 +6,24 @@ class enumOperator(object):
     def read_name_file(self):
         with open(self.name_filename, 'r') as f:
             return f.read()
+
+    def gen_c_file(self):
+        with open(self.name_filename, 'r') as name_file:
+            c_file_name = name_file.readline().split('\n')[0]
+            with open(c_file_name + '.c', 'w+') as c_file:
+                c_file.write(
+                    'extern const char*f NAME_names[];\ntypedef enum {'
+                )
+
+                name_data = name_file.read().split('\n')
+
+                if len(name_data) > 2:
+                    c_file.write('\n\t' + name_data[0])
+                    for name in name_data[1:-1]:
+                        name = name.replace('\n', '')
+                        c_file.write(',\n\t' + name)
+                c_file.write('\n} NAME;\r\n')
+
+    def read_c_file(self, name):
+        with open(name, 'r') as f:
+            return f.read()
--- pragmatic_programmar/test_ex19_11.py
+++ pragmatic_programmar/test_ex19_11.py
@@ -10,3 +10,9 @@ class TestName(unittest.TestCase):
     def test_read_name_file(self):
         d = self.e.read_name_file()
         self.assertEqual(d, "name\nstate_a\nstate_b\nstate_c\nstate_d\nstate_e\nstate_f\n")
+
+    @unittest.skip('needed correct test text')
+    def test_generate_c_file_from_name_file(self):
+        self.e.gen_c_file()
+        d = self.e.read_c_file('name.c')
+        self.assertEqual(d, "extern const char*f NAME_names[];\ntypedef enum {\n\tstate_a,\n\tstate_b,\n\tstate_c,\n\tstate_d\n\tstate_e\n\tstate_f\n} NAME;\n")
